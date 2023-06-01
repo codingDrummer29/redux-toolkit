c6aae41b492c86c6c800bd33285465fb2f09fc47
Anubhab Mukherjee <anubhab@teks.co.in>
2023-06-01 13:48:24 +0530

    basic project setup: - clean setup - tailwindcss and sass configuration - required packages installed

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen test">
+        <span>hi</span>
+      </div>
+    </>
+  );
+};
+
+export default App;
