71f4f52839d6b60ee7d7f37f139ef0f0916d5612
Anubhab Mukherjee <anubhab@teks.co.in>
2023-06-02 15:27:46 +0530

    update: - search functionality added - UI modified

--- src/common/constant.js
+++ src/common/constant.js
@@ -3,6 +3,7 @@ export const BASE_URL = "http://www.omdbapi.com";
 export const POSTER_URL = "http://img.omdbapi.com";
 
 export const SEARCH_TYPES = [
+  { name: "--- Select ---", label: "--- Select ---", value: "" },
   { name: "Movies", label: "Movie", value: "movie" },
   { name: "Series", label: "Series", value: "series" },
   { name: "Episode", label: "Episode", value: "episode" },
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 
@@ -8,15 +8,17 @@ import { fetchAsyncMovies } from "../store/features/movies/movieSlice";
 // import { addMovies } from "../store/features/movies/movieSlice";
 
 const Home = () => {
-  const [search, setSearch] = useState("harry");
-  const [type, setType] = useState(SEARCH_TYPES[0].value);
+  // const [search, setSearch] = useState("harry");
+  // const [type, setType] = useState("");
 
+  const searchInput = useRef();
+  const searchType = useRef();
   const dispatch = useDispatch();
   const Navigate = useNavigate();
 
   useEffect(() => {
     if (window.location.pathname === "/") Navigate("/movies");
-    dispatch(fetchAsyncMovies({ search, type }));
+    // dispatch(fetchAsyncMovies({ search, type }));
 
     // fetchMovies();
 
@@ -24,7 +26,8 @@ const Home = () => {
     //   setSearch("");
     //   setType("");
     // };
-  }, [dispatch, Navigate, search, type]);
+  }, [dispatch, Navigate]);
+  // }, [dispatch, Navigate, search, type]);
 
   // const fetchMovies = async () => {
   //   const response = await movieApi
@@ -35,10 +38,54 @@ const Home = () => {
   //     dispatch(addMovies(response.data.Search));
   // };
 
+  const handleSearch = () => {
+    let search = searchInput.current.value ?? "",
+      type = searchType.current.value;
+
+    if (search.trim() !== "") {
+      if (type.trim() !== "") {
+        dispatch(fetchAsyncMovies({ search, type }));
+      } else alert("Please choose a type");
+    } else alert("Please enter a name");
+  };
+
   return (
     <>
       {/* <Header /> */}
       <main className="px-8 py-4 w-full min-h-body bg-sky-950 text-white">
+        {/* --- Search | type --- */}
+        <div className="flex justify-center items-center gap-8 h-20">
+          <div className="w-44 h-8">
+            <input
+              className="rounded bg-sky-900 text-white outline-1 outline-sky-600 p-2"
+              placeholder="Type to search..."
+              ref={searchInput}
+              type="text"
+            />
+          </div>
+          <div className="w-44 h-8 flex items-center gap-2">
+            <select
+              className="rounded bg-sky-900 text-shite outline-1 outline-sky-600 p-2"
+              ref={searchType}
+            >
+              {SEARCH_TYPES.map((item, index) => {
+                return (
+                  <option key={index} value={item.value}>
+                    {item.name}
+                  </option>
+                );
+              })}
+            </select>
+            <button
+              onClick={handleSearch}
+              className="px-4 py-2 bg-sky-900 border border-sky-700 txt-sm text-white"
+            >
+              Search
+            </button>
+          </div>
+        </div>
+
+        {/* --- Components --- */}
         <Outlet />
       </main>
     </>
